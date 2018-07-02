1ca08bacf23558e43ff0bd801cfd1ebe94b6ba64
Zidane Wright <zidanewright@outlook.com>
2018-07-02 12:09:52 -0500

    simplify audience rating

--- app/src/main/java/com/pluto/models/AudienceRating.java
+++ app/src/main/java/com/pluto/models/AudienceRating.java
@@ -1,21 +1,11 @@
 package com.pluto.models;
 
 public enum AudienceRating {
-    G("General Audiences"),
-    PG("Parental Guidance Suggested"),
-    PG13("Parents Strongly Cautioned"),
-    R("Restricted"),
-    A18("Adults 18 and Over"),
-    T16("Teen 16"),
-    NC17("Adults Only");
-
-    private String longName;
-
-    public String getLongName() {
-        return this.longName;
-    }
-
-    AudienceRating(String longName) {
-        this.longName = longName;
-    }
+    G,
+    PG,
+    PG13,
+    R,
+    A18,
+    T16,
+    NC17;
 }
