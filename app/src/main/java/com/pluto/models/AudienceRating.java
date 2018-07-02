7a8f0555c77d99ebb3d23e2362dc7ceaef0046d1
Zidane Wright <zidanewright@outlook.com>
2018-07-02 12:16:30 -0500

    add comments for audience rating enum values

--- app/src/main/java/com/pluto/models/AudienceRating.java
+++ app/src/main/java/com/pluto/models/AudienceRating.java
@@ -1,11 +1,11 @@
 package com.pluto.models;
 
 public enum AudienceRating {
-    G,
-    PG,
-    PG13,
-    R,
-    A18,
-    T16,
-    NC17;
+    G, // General Audiences
+    PG, // Parental Guidance Suggested
+    PG13, // Parental Guidance for children under 13
+    R, // Restricted - under 17 requires accompanying parent or adult
+    A18, // Adults 18 and over
+    T16, // Teens 16 and over
+    NC17 // No children under 17
 }
